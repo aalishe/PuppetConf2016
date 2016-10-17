1319871c0d64ad7771edad9897f81c2d86daee3e
Johandry Amador <johandry@yahoo.com>
2016-10-16 23:26:08 -0700

    Requirements script output improved

--- requirements.sh
+++ requirements.sh
@@ -3,11 +3,11 @@
 mkdir -p ./files
 
 [[ ! -e ./files/puppet-student.ova ]] && wget -O ./files/puppet-student.ova http://downloads.puppet.com/training/puppet-student.ova
-[[ -e ./files/puppet-student.ova ]] && echo "PuppetConf 2016 Student OVA file was downloaded"
+[[ -e ./files/puppet-student.ova ]] && echo -e "\x1B[92;1mPuppetConf 2016 Student OVA file was downloaded\x1B[0m"
 
 VBoxManage list vms | grep -q student || VBoxManage import files/puppet-student.ova 2>&1 >/dev/null
-VBoxManage list vms | grep -q student && echo "PuppetConf 2016 Student VM was imported into VirtualBox"
+VBoxManage list vms | grep -q student && echo -e "\x1B[92;1mPuppetConf 2016 Student VM was imported into VirtualBox\x1B[0m"
 
-VBoxManage list runningvms | grep -q student || VBoxManage startvm student
-VBoxManage list runningvms | grep -q student && echo "PuppetConf 2016 Student VM is running" && \
-echo -e "Use SSH to log in: \n  ssh root@10.0.2.15\nView the VM in VirtualBox to know the randomly generated password"
+VBoxManage list runningvms | grep -q student || VBoxManage startvm student 2>&1 >/dev/null
+VBoxManage list runningvms | grep -q student && echo -e "\x1B[92;1mPuppetConf 2016 Student VM is running\x1B[0m" && \
+echo -e "Use SSH to log in: \n  \x1B[92;1mssh root@10.0.2.15\x1B[0m\nView the VM in VirtualBox to know the randomly generated password"
